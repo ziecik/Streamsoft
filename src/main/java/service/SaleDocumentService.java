a90d8263b72f6832c7d0276189eb61ce939017d0
ziecik <jaroslaw.ziecik@gmail.com>
2021-03-16 20:59:40 +0100

    task1 modified

--- src/main/java/service/SaleDocumentService.java
+++ src/main/java/service/SaleDocumentService.java
@@ -10,9 +10,9 @@ public class SaleDocumentService {
 	
 	public void insert() {
 		
-//		BigDecimal valueConvertedToPLN = CurrencyUtil.convertToPLN(new BigDecimal("127"), CurrencyCode.GBP, LocalDate.parse("2019-10-10"));
+		BigDecimal valueConvertedToPLN = CurrencyUtil.convertToPLN(new BigDecimal("127"), CurrencyCode.GBP, LocalDate.parse("2019-10-10"));
 
-		 CurrencyUtil.getMinValue(CurrencyCode.USD, LocalDate.parse("2012-10-11"), LocalDate.parse("2013-05-05"));
+//		 CurrencyUtil.getMinValue(CurrencyCode.USD, LocalDate.parse("2012-10-11"), LocalDate.parse("2013-05-05"));
 
 		
 	}
